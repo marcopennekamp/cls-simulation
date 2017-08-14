68d0346a9f87f19c9ddb729f78ad33438457be9b
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-08-14 13:03:33 +0200

    Simplify actions.

--- src/main/scala/simulation/automaton/runner/RunnerRepository.scala
+++ src/main/scala/simulation/automaton/runner/RunnerRepository.scala
@@ -8,16 +8,17 @@ trait RunnerRepository extends AutomatonCombinators {
 
   @combinator object RunRunner extends Run[State]('grnd)
   @combinator object FinGrnd extends Fin('grnd)(PlayerPosition.startState)
-
-  @combinator object GrndG extends Transition('grnd, 'g(_), 'grnd)(Action.run)
-  @combinator object GrndB extends Transition('grnd, 'b(_), 'air1)(Action.jump)
-  @combinator object Air1G extends Transition('air1, 'g(_), 'grnd)(Action.fall)
-  @combinator object Air1B extends Transition('air1, 'b(_), 'air2)(Action.jump)
-  @combinator object Air2G extends Transition('air2, 'g(_), 'fall)(Action.fall)
-  @combinator object Air2B extends Transition('air2, 'b(_), 'dead)(Action.die)
-  @combinator object FallG extends Transition('fall, 'g(_), 'grnd)(Action.fall)
-  @combinator object FallB extends Transition('fall, 'b(_), 'dead)(Action.die)
-  @combinator object DeadG extends Transition('dead, 'g(_), 'dead)(Action.stayDead)
-  @combinator object DeadB extends Transition('dead, 'b(_), 'dead)(Action.stayDead)
+  
+  import Action._
+  @combinator object GrndG extends Transition('grnd, 'g(_), 'grnd)(run)
+  @combinator object GrndB extends Transition('grnd, 'b(_), 'air1)(jump)
+  @combinator object Air1G extends Transition('air1, 'g(_), 'grnd)(fall)
+  @combinator object Air1B extends Transition('air1, 'b(_), 'air2)(jump)
+  @combinator object Air2G extends Transition('air2, 'g(_), 'fall)(fall)
+  @combinator object Air2B extends Transition('air2, 'b(_), 'dead)(die)
+  @combinator object FallG extends Transition('fall, 'g(_), 'grnd)(fall)
+  @combinator object FallB extends Transition('fall, 'b(_), 'dead)(die)
+  @combinator object DeadG extends Transition('dead, 'g(_), 'dead)(stayDead)
+  @combinator object DeadB extends Transition('dead, 'b(_), 'dead)(stayDead)
 
 }
